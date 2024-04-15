5fefeb711ec1ba39e50ea11b255580810f81efe4
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-15 15:55:09 -0300

    Adicionando métodos ToString e ValorTotalEmEstoque

---
+++ ClassesEx2/ClassesEx2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + produto);
+        }
+    }
+}
