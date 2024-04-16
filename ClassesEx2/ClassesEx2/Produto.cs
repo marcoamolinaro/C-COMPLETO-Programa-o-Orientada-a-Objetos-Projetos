7f28ed502943f100c907a30a0a1a144ab738a2f5
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-16 10:41:54 -0300

    Imprementação de auto properties

--- ClassesEx2/ClassesEx2/Produto.cs
+++ ClassesEx2/ClassesEx2/Produto.cs
@@ -6,8 +6,8 @@ namespace ClassesEx2
     internal class Produto
     {
         private string? _nome;
-        private double _preco;
-        private int _quantidade;
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
 
         public Produto()
         {
@@ -16,15 +16,15 @@ namespace ClassesEx2
         public Produto(string nome, double preco, int quantidade)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = quantidade;
+            Preco = preco;
+            Quantidade = quantidade;
         }
 
         public Produto(string nome, double preco)
         {
             _nome = nome;
-            _preco = preco;
-            _quantidade = 5;
+            Preco = preco;
+            Quantidade = 5;
         }
 
         public string Nome
@@ -40,38 +40,28 @@ namespace ClassesEx2
             }
         }
 
-        public double Preco
-        {
-            get { return _preco; }
-        }
-
-        public int Quantidade
-        {
-            get { return _quantidade;}
-        }
-
         public double ValorTotalEmEstoque()
         {
-            return _preco * _quantidade;
+            return Preco * Quantidade;
         }
 
         public void AdicionarProdutos(int quantiade)
         {
-            _quantidade += quantiade;
+            Quantidade += quantiade;
         }
 
         public void RemoverProdutos(int quantidade)
         {
-            _quantidade -= quantidade;
+            Quantidade -= quantidade;
         }
 
         public override string ToString()
         {
             return _nome
                 + ", $ "
-                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
                 + ", "
-                + _quantidade
+                + Quantidade
                 + " unidades, Total: $ "
                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
