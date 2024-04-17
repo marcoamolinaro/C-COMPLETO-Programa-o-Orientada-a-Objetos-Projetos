c287172694b5615c7a0449f32b623836a576c09d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-17 17:27:13 -0300

    Projeto vetores calcula média do produto

---
+++ VetoresApp1/VetoresApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] produtos = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produtos[i] = new Produto
+                {
+                    Nome = nome,
+                    Preco = preco
+                };
+            }
+
+            double soma = 0.0;
+
+            for (int i = 0;i < n;i++)
+            {
+                soma += produtos[i].Preco;
+            }
+            
+            double avg = soma / n;
+
+            Console.WriteLine("Preço médio = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+
+        }
+    }
+}
