f696bdef6490848e0615ee3250bccef0f3fa702d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-23 10:24:49 -0300

    Enumerações

---
+++ EnumApp1/EnumApp1/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EnumApp1.Entities.Enums;
+
+namespace EnumApp1.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ EnumApp1/EnumApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using EnumApp1.Entities;
+using EnumApp1.Entities.Enums;
+
+namespace EnumApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+
+            Console.WriteLine(os);
+        }
+    }
+}
