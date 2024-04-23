bba6e0b33fdd7587e1032e75f1e9629268f3fa98
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-23 10:59:34 -0300

    Fist commit

---
+++ ContractWorkApp1/ContractWorkApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractWorkApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
