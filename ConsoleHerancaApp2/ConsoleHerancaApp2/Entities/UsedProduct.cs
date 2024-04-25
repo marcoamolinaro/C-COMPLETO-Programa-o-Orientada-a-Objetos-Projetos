06b7833fb0481c15cd912a7877066ccca01d20a6
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-04-24 21:25:14 -0300

    Final commit, exercice inheritance.

---
+++ ConsoleHerancaApp2/ConsoleHerancaApp2/Entities/UsedProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace ConsoleHerancaApp2.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string priceTag()
+        {
+            return Name + " (used) $ " + Price.ToString("F2", CultureInfo.InvariantCulture) + " (Manufacture date " + ManufactureDate.ToString("dd/MM/yyyy") + ")";
+        }
+    }
+}
---
+++ ConsoleHerancaApp2/ConsoleHerancaApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ConsoleHerancaApp2.Entities;
+
+namespace ConsoleHerancaApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported(c / u / i) ? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'c')
+                {
+                    products.Add(new Product(name, price));
+                }
+                else if (ch == 'i') {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    ImportedProduct imp = new ImportedProduct(name, price, customsFee);
+                    imp.TotalPrice();
+                    products.Add(imp);
+                }
+                else 
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    string manufactureDate = Console.ReadLine();
+                    UsedProduct used = new UsedProduct(name, price, DateTime.Parse(manufactureDate));
+                    products.Add(used);
+                }
+            }
+
+            Console.WriteLine("PRICE TAGS: ");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.priceTag());
+            }
+        }
+    }
+}
