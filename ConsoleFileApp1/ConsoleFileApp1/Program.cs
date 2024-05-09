90597b2a3960a7b058967ccdb2e2300a722c9440
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-09 17:24:50 -0300

    File and FileInfo examples

---
+++ ConsoleFileApp1/ConsoleFileApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleFileApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath =
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\file1.txt";
+
+            string targetPath =
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\file2.txt";
+
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException ioe) { 
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(ioe.Message);
+            }
+        }
+    }
+}
