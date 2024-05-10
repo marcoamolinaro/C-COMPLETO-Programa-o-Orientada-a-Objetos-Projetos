832b24dd74703b1127f31d96d5063ae12b78c9c5
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-10 10:27:01 -0300

    Second example using File.OpenText()

--- ConsoleBlockUsingApp1/ConsoleBlockUsingApp1/Program.cs
+++ ConsoleBlockUsingApp1/ConsoleBlockUsingApp1/Program.cs
@@ -7,24 +7,21 @@ namespace ConsoleBlockUsingApp1
     {
         static void Main(string[] args)
         {
-            string path = 
+            string path =
                 @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\file1.txt";
 
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    using (StreamReader sr = new StreamReader(fs))
+                    while (!sr.EndOfStream)
                     {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
                     }
                 }
             }
-            catch(IOException ioe)
+            catch (IOException ioe)
             {
                 Console.WriteLine("An error occurred!");
                 Console.WriteLine(ioe.Message);
