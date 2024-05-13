55ab4f70b24f07c4ac91602a92453af0dc44c31c
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-13 09:55:52 -0300

    StreamWriter example

---
+++ ConsoleStreamWriterApp1/ConsoleStreamWriterApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleStreamWriterApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath =
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\file1.txt";
+            string targetPath =
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
