b4e24daedc80741d95f8d28146fd92dad3886c80
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-13 11:12:59 -0300

    Path examples

---
+++ ConsolePathApp1/ConsolePathApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ConsolePathApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = 
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\myfolder\note1.txt";
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+        }
+    }
+}
