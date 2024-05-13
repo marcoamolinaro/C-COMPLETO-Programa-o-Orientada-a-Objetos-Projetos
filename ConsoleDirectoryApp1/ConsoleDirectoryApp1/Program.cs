e75c04cb09e23ba8436508f93cf1bd8a13f334a9
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-13 10:11:01 -0300

    First example

---
+++ ConsoleDirectoryApp1/ConsoleDirectoryApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleDirectoryApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path =
+                @"D:\Desenvolvedor\Udemy\C#_COMPLETO_Programação_Orientada _a_Objetos_Projetos\temp\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
