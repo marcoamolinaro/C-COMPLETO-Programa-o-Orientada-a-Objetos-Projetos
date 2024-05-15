e9ff339957529772a00014c712b8740e5ea95d4e
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-15 10:36:18 -0300

    Fisrt commit

---
+++ ConsolegGenericsApp1/ConsolegGenericsApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsolegGenericsApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.Write("First: " + printService.First());
+
+
+        }
+    }
+}
