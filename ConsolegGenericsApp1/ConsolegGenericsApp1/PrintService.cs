b03a58e5ad1acb8e299a8a3273446154e2345b69
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-15 10:47:36 -0300

    Implementing Generics

--- ConsolegGenericsApp1/ConsolegGenericsApp1/PrintService.cs
+++ ConsolegGenericsApp1/ConsolegGenericsApp1/PrintService.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace ConsolegGenericsApp1
 {
-    class PrintService
+    class PrintService<T>
     {
-        private int[] _values = new int[10];
+        private T[] _values = new T[10];
         private int _count = 0;
 
-        public void AddValue(int value)
+        public void AddValue(T value)
         {
             if (_count == 10)
             {
@@ -18,7 +18,7 @@ namespace ConsolegGenericsApp1
             _count++;
         }
 
-        public int First()
+        public T First()
         {
             if (_count == 0)
             {
--- ConsolegGenericsApp1/ConsolegGenericsApp1/Program.cs
+++ ConsolegGenericsApp1/ConsolegGenericsApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            PrintService printService = new PrintService();
+            PrintService<int> printService = new PrintService<int>();
             Console.Write("How many values? ");
             int n = int.Parse(Console.ReadLine());
 
