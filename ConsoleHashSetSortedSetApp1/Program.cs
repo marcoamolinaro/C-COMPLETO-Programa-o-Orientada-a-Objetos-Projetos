e201a7dd9c090dd3085fe42eecb12295ffcf4d30
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-16 12:59:28 -0300

    Implementing HashSet example

---
+++ ConsoleHashSetSortedSetApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleHashSetSortedSetApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (string item in set)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
