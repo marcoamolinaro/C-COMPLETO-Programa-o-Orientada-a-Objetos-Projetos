e5cf7165f09122865adb69d5f4fd216cd6048d14
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-16 12:48:22 -0300

    Implementing Equals and GetHashCode

---
+++ ConsoleEqualsGetHashCodeApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleEqualsGetHashCodeApp1.Entities;
+
+namespace ConsoleEqualsGetHashCodeApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Marco", Email = "marco@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+        }
+    }
+}
