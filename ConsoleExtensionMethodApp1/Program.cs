a96d73209518608197b501072b3e0a2f30101619
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-16 15:47:19 -0300

    Adding StringExtensions

--- ConsoleExtensionMethodApp1/Program.cs
+++ ConsoleExtensionMethodApp1/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleExtensionMethodApp1
         {
             DateTime dt = new DateTime(2024, 05, 16, 8, 10, 45);
             Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students";
+            Console.WriteLine(s1.Cut(10));
         }
     }
 }
