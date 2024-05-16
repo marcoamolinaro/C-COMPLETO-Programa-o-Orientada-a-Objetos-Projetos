43678578fa6227a3c88bd353a673e074a77e3d8d
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-16 15:40:58 -0300

    First example

---
+++ ConsoleExtensionMethodApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConsoleExtensionMethodApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 05, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
