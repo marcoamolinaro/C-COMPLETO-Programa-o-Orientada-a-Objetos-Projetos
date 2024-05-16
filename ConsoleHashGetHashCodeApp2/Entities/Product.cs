10f7805d015ca26f63495347f865e7d23daa03f5
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-16 13:40:21 -0300

    Implementing comparation with Class and Struct

---
+++ ConsoleHashGetHashCodeApp2/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ConsoleHashGetHashCodeApp2.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Product product)) return false;   
+            
+            Product other = obj as Product;
+
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
