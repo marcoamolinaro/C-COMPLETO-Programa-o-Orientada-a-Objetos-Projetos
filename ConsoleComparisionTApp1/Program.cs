b8588960745e2e8fbf84262fc01a17571129baca
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-17 10:11:25 -0300

    Implement comparison with Lambda expression and uses it as Sort function's argument

--- ConsoleComparisionTApp1/Program.cs
+++ ConsoleComparisionTApp1/Program.cs
@@ -14,9 +14,7 @@ namespace ConsoleComparisionTApp1
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            list.Sort(comp);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product p in list)
             {
