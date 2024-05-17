fb8aa821d48b023b27955501ea07d612dbf311a8
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-17 10:09:15 -0300

    Implementing comparison using Lambda expression

--- ConsoleComparisionTApp1/Program.cs
+++ ConsoleComparisionTApp1/Program.cs
@@ -14,17 +14,14 @@ namespace ConsoleComparisionTApp1
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort(CompareProducts);
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort(comp);
 
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-        }
-
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
