31aa886b953e55195628244a0afe4abbfce0932e
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-17 16:44:15 -0300

    Implementing delegate

--- ConsoleDelegateApp1/Program.cs
+++ ConsoleDelegateApp1/Program.cs
@@ -1,7 +1,12 @@
-﻿using ConsoleDelegateApp1.Services;
+﻿using System;
+using ConsoleDelegateApp1.Services;
 
 namespace ConsoleDelegateApp1
 {
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+    delegate double BinaryNumericOperation1(double n1);
+
     class Program
     {
         static void Main(string[] args)
@@ -9,13 +14,17 @@ namespace ConsoleDelegateApp1
             double a = 10;
             double b = 12;
 
-            double result = CalculationService.Sum(a, b);
+            BinaryNumericOperation opSum = CalculationService.Sum;
+            BinaryNumericOperation opMax = CalculationService.Max;
+            BinaryNumericOperation1 opSquare = CalculationService.Square;
+
+            double result = opSum(a, b);
             Console.WriteLine(result);
 
-            result = CalculationService.Max(a, b);
+            result = opMax(a, b);
             Console.WriteLine(result);
 
-            result = CalculationService.Square(a);
+            result = opSquare(a);
             Console.WriteLine(result);
         }
     }
