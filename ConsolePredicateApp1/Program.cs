d20b4ba0aaf67a69925fc1c43c7424b3f54ef352
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-17 17:02:26 -0300

    First commit

---
+++ ConsolePredicateApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsolePredicateApp1.Entities;
+
+namespace ConsolePredicateApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.00);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
