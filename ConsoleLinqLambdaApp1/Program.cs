0a777d39604643947ed11488d088664d117e345c
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-20 15:58:36 -0300

    Where, OrderBy and ThenBy

--- ConsoleLinqLambdaApp1/Program.cs
+++ ConsoleLinqLambdaApp1/Program.cs
@@ -47,6 +47,10 @@ namespace ConsoleLinqLambdaApp1
 
             var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new {p.Name, p.Price, CategoryName = p.Category.Name});
             Print("NAMES STARTED WITH 'C' AND ANONYMOUS OBJECT", r3);
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
         }
     }
 }
