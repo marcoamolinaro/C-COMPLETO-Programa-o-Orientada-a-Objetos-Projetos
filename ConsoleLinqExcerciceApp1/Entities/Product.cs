b14b2809b9456b5a7883e145474dab88cd9e024f
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-20 17:57:39 -0300

    first commit

---
+++ ConsoleLinqExcerciceApp1/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleLinqExcerciceApp1.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+    }
+}
