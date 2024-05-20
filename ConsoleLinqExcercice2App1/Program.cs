ab09a91fe3a617a15decee4255358eec6024176c
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-20 18:25:51 -0300

    First commit

---
+++ ConsoleLinqExcercice2App1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+using ConsoleLinqExcercice2App1.Entities;
+
+namespace ConsoleLinqExcercice2App1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+
+                        list.Add(new Employee(name, email, salary));
+                    }
+
+                    var emails = list.Where(e => e.Salary > limit).OrderBy(e => e.Email).Select(e => e.Email);
+
+                    var sum = list.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+
+                    Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                    foreach (string email in emails)
+                    {
+                        Console.WriteLine(email);
+                    }
+                    Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
