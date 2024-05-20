d887641939dc0921b985c94bcd56588772578ca0
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-05-20 15:08:13 -0300

    Fisrt commit

---
+++ ConsoleLinqApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleLinqApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            // Execute the Query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
