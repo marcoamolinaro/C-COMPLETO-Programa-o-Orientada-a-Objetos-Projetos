676f97783bbb8b242770946e014d8b3b96b824dc
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2024-06-10 09:42:20 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -30,6 +30,7 @@ namespace SalesWebMvc
 
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
+            builder.Services.AddScoped<DepartmentService>();
 
             var app = builder.Build();
 
